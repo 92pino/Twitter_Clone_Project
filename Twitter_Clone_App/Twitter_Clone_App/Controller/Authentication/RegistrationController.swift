6880edd245e948904d9a54181d067c072bc261a8
92pino <jjb8382@gmail.com>
2020-03-27 00:33:19 +0900

    :art:Login View, Extension:art:
    - Create Login View
    - Login textField, signUp Button add Extension

---
+++ Twitter_Clone_App/Twitter_Clone_App/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,29 @@
+//
+//  RegistrationController.swift
+//  Twitter_Clone_App
+//
+//  Created by JinBae Jeong on 2020/03/26.
+//  Copyright © 2020 pino. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+
+        // MARK: - Properties
+        
+        // MARK: - Lifecycle
+        override func viewDidLoad() {
+            super.viewDidLoad()
+
+            configureUI()
+        }
+        
+        // MARK: - Slectors
+        
+        // MARK: - Helpers
+        func configureUI() {
+            view.backgroundColor = .twitterBlue
+        }
+
+    }
