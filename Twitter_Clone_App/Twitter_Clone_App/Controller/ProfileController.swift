d6144bc8491a948f73a1f8c675ffbecbbca186f2
92pino <jjb8382@gmail.com>
2020-03-31 23:03:28 +0900

    :tada:ProfileController add:tada:

---
+++ Twitter_Clone_App/Twitter_Clone_App/Controller/ProfileController.swift
@@ -0,0 +1,52 @@
+//
+//  ProfileController.swift
+//  Twitter_Clone_App
+//
+//  Created by JinBae Jeong on 2020/03/30.
+//  Copyright © 2020 pino. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "TweetCell"
+
+class ProfileController: UICollectionViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureCollectionView()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureCollectionView() {
+        collectionView.backgroundColor = .white
+        collectionView.register(TweetCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension ProfileController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TweetCell
+        
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension ProfileController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 120)
+    }
+}
